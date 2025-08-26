2d10dff348544f39caeccd1520d6299f289e4014
Yasuhiro Yanagawa <yngw.811@gmail.com>
2025-08-26 15:28:49 +0900

    インデント修正

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const Basic1 = (props) => {
-  const clickHandler = () => {
-    console.log('clicked')
-  }
+    const clickHandler = () => {
+        console.log('clicked')
+    }
 
-  return (
-    <div>
-      <button onClick={clickHandler}>Click</button>
-      <h1>Hello { props.name }</h1>
-      <h1>Hello React2</h1>
-    </div>
-  )
+    return (
+        <div>
+            <button onClick={clickHandler}>Click</button>
+            <h1>Hello { props.name }</h1>
+            <h1>Hello React2</h1>
+        </div>
+    )
 }
 
 export default Basic1
